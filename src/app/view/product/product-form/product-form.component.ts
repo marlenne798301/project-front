125b8e32bd38463d88b646f9e3a62f8e1a32b85a
Marlenne <Marlenneibarr@gmail.com>
2024-06-06 22:13:06 -0600

    v1

--- src/app/view/product/product-form/product-form.component.ts
+++ src/app/view/product/product-form/product-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ProductListComponent } from '../product-list/product-list.component';
+import { Product } from 'src/app/model/products';
 
 @Component({
   selector: 'app-product-form',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
 
-  constructor() { }
+  formGroup!:FormGroup
+
+  constructor(public dialogRef: MatDialogRef<ProductListComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Product,
+    private formBuilder:FormBuilder) {
+
+   }
 
   ngOnInit(): void {
+    this.initForm
+  }
+
+  initForm(){
+    if(!this.data) {
+      this.formGroup=this.formBuilder.group({
+        name:["", Validators.required],
+        code:["", Validators.required],
+        category:["", Validators.required],
+        description:["", Validators.required],
+        price:["", Validators.required],
+        amount:["", Validators.required]
+      });
+
+    }else{
+      this.formGroup=this.formBuilder.group({
+        name:[this.data.name|| "", Validators.required ],
+        code:[this.data.name|| "", Validators.required ],
+        category:[this.data.name|| "", Validators.required ],
+        description:[this.data.name|| "", Validators.required ],
+        price:[this.data.name|| "", Validators.required ],
+        amount:[this.data.name|| "", Validators.required ],
+      })
+    }
   }
 
 }
